ce1179c7769a160cdda22d592770e4136a32dbc4
Eiji Tanioka <staff_ag@ping-t.com>
2023-06-07 23:20:02 +0900

    アブストラクト、場所、開催日を取得

--- janov-program-csv.py
+++ janov-program-csv.py
@@ -35,14 +35,33 @@ def program_info(url):
 
         # get Program title
         title = soup.h1.string
+        print(title)
 
-        # get content list
+        # get abstract
         content = soup.find('div', attrs={'class', 'entry-content cf'})
-        #print(content.h3.find_next_siblings('p'))
+        content_abstract = content.find(id='toc1')
+        abstract = content_abstract.find_next()
+        abstract_text = ""
+        while abstract.name == 'p':
+            abstract_text += abstract.text + '\n'
+            abstract = abstract.find_next()
+            if abstract.name == 'h3':
+                break
 
+        #print(abstract_text)
 
+        # get place
+        content_place = content.find(id='toc2')
+        place = content_place.find_next('p').text
+        #print(place)
+
+        # get hold date
+        content_date = content.find(id='toc3')
+        hold_date = content_date.find_next('p').text.split(' ')[1]
+        print(hold_date)
 
 if __name__ == '__main__':
     links = get_program_list(url)
     for link in links:
         program_info(link)
+        break
