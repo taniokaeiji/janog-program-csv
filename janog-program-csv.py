5ce78935ee419a53a5647c7eb757ae6eaaf091c9
Eiji Tanioka <etanioka@cisco.com>
2024-06-25 17:30:34 +0900

    JANOG54のカレンダーエントリに使用したもの

--- janog-program-csv.py
+++ janog-program-csv.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 
 import datetime, locale, csv
 
-url = 'https://www.janog.gr.jp/meeting/janog52/program-ja-timeline/'
+#url = 'https://www.janog.gr.jp/meeting/janog53/program-ja-timeline/'
+url = 'https://www.janog.gr.jp/meeting/janog54/program-ja-timetable/'
 
 def get_program_list(url):
     link_list = []
@@ -11,10 +12,12 @@ def get_program_list(url):
     res = requests.get(url)
     content = res.content
     soup = BeautifulSoup(content, 'html.parser')
-    for link in soup.find_all('div', 'timeline-item-snippet'):
-        if link.a is None:
-            continue
-        link_list.append(link.a["href"])
+    #for link in soup.find_all('div', 'timeline-item-snippet'):
+    for link in soup.find_all('a'):
+        #if link.a is None:
+        #    continue
+        if "https://www.janog.gr.jp" in link["href"]:
+            link_list.append(link["href"])
 
     return list(set(link_list))
 
@@ -23,8 +26,11 @@ def program_info(url):
     content = res.content
     soup = BeautifulSoup(content, 'html.parser')
 
-    meta_keywords = soup.find_all('meta', attrs={'name': 'keywords'})
-    if meta_keywords[0].get('content') != 'JANOGプログラム':
+    meta_keywords = soup.find('meta', attrs={'name': 'keywords', 'content': True})
+    type = soup.find('meta', attrs={"property": "og:type"})
+    if meta_keywords is None or meta_keywords['content'] != 'JANOGプログラム':
+        exit
+    elif type["content"] != "article":
         exit
     else:
         # keep page url
@@ -33,6 +39,7 @@ def program_info(url):
         # get Time, Presenter
         meta_description = soup.find('meta', attrs={'name': 'description'})
         description = meta_description.get('content')
+        
         presenter, start_time, end_time = description.split('|')
         presenter = presenter.replace(')', ')\n')
 
@@ -41,32 +48,42 @@ def program_info(url):
 
         # get abstract
         content = soup.find('div', attrs={'class', 'entry-content cf'})
-        content_abstract = content.find(id='toc1')
+        content_abstract = content.find(name="span", string="概要")
         abstract = content_abstract.find_next()
         abstract_text = ""
-        while abstract.name == 'p':
-            abstract_text += abstract.text + '\n'
-            abstract = abstract.find_next()
+        while True:
             if abstract.name == 'h3':
                 break
-            while abstract.name == 'br':
+            elif abstract.name == 'h4':
+                abstract_text += '\n' + abstract.text + '\n'
+            elif abstract.name == 'p':
+                abstract_text += abstract.text + '\n'
+            elif abstract.name == 'li':
+                abstract_text += "・" + abstract.text + '\n'
+            #elif abstract.name == 'br':
                 #abstract_text += '\n'
-                abstract = abstract.find_next()
+                #abstract = abstract.find_next()
+            elif abstract.name == 'a':
+                # if abstract.text == abstract.attrs["href"]:
+                #     abstract_text += abstract.attrs["href"] + '\n'
+                # else:    
+                    abstract_text += abstract.text + ' ' + abstract.attrs["href"] + '\n'
+            abstract = abstract.find_next()
             
-            abstract_text += '\n'
-
+        abstract_text += '\n'
+    
         abstract = link + ""
         abstract += "\n\n■概要\n"
         abstract += abstract_text
-        abstract += "\n■登壇者\n"
+        abstract += "■登壇者\n"
         abstract += presenter
 
         # get place
-        content_place = content.find(id='toc2')
+        content_place = content.find(name="span", string="場所")
         place = content_place.find_next('p').text
 
         # get hold date
-        content_date = content.find(id='toc3')
+        content_date = content.find(name="span", string="日時")
         hold_date_text = content_date.find_next('p').text.split(' ')[1]
         locale.setlocale(locale.LC_TIME, 'ja_JP.UTF-8')
         hold_date = datetime.datetime.strptime(hold_date_text, '%Y年%m月%d日(%a)').strftime('%Y/%m/%d')
